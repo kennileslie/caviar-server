9b6756750ee7d991b346bd05749d4482a7d9b8e4
dev-apiarist <klesliedev@gmail.com>
2022-09-04 23:47:56 -0500

    .

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const AppError = require('./utils/appError');
 const app = express();
 
 app.use(cors('*'));
-app.get('', (req, res) => {
+app.get('/', (req, res) => {
   res.json({ name: 'Caviar Honey API', v: 'v0.0.0' });
 });
 
@@ -22,7 +22,6 @@ if (process.env.NODE_ENV !== 'production') app.use(morgan('dev'));
 
 app.use('/api/v1/items', itemRouter);
 app.use('/api/v1/users', userRouter);
-app.get('/', (_, res) => res.sendStatus(200));
 
 app.all('*', (_req, _res, next) => next(new AppError('Url not found', 404)));
 app.use(globalErrorHandler);
