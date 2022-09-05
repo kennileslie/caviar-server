5f254be4a6e5ab557c696139adbc6e002e5ed997
dev-apiarist <klesliedev@gmail.com>
2022-09-04 22:34:11 -0500

    bugfix

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 const app = require('./app');
 
 mongoose
-	.connect(process.env.DB_URL)
-	.then(() => console.log('Connected to database...'))
-	.catch((err) => {
-		console.err(err);
-		process.exit(1);
-	});
+  .connect(process.env.DB_URL)
+  .then(() => console.log('Connected to database...'))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listen on port ${port}...`));
