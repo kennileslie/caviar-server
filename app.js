66f21a0a235b52708989fdc5131f68d56b67e7ad
dev-apiarist <klesliedev@gmail.com>
2023-03-20 16:13:44 -0500

    fix: cors origin specified

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const AppError = require('./utils/appError');
 
 const app = express();
 
-app.use(cors('https://caviar-honey.vercel.app'));
+app.use(cors({ origin: 'https://caviar-honey.vercel.app' }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
